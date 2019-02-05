c157fde2ae6cb944187e7538b26411df6181292a
Cooconuts <45293763+Cooconnuts@users.noreply.github.com>
2019-02-05 18:41:55 +0100

    Animación hielo y script

---
+++ Fluff!/Assets/Scripts/Hielo.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hielo : MonoBehaviour
+{
+    Animator animacion;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animacion = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag ("Pelusa"))
+        {
+
+            Destroy (gameObject);
+        }
+    }
+}
