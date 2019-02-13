d1a3ab90f56c826d05ec738d990f2187d9cefce2
Viejastirpe <danieljq1995@gmail.com>
2019-02-13 16:39:27 +0100

    Botones

---
+++ Fluff!/Assets/Scripts/BotonMute.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BotonMute : MonoBehaviour
+{
+	public Sprite OffSprite;
+	public Sprite OnSprite;
+	public Button but;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	public void ChangeImage(){
+		if (but.image.sprite == OnSprite)
+			but.image.sprite = OffSprite;
+		else {
+			but.image.sprite = OnSprite;
+		}
+	}
+}
