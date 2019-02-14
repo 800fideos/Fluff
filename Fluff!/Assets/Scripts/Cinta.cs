7228122a629161ddd94142ac4ba552b59b48da46
Cooconuts <rafikyzhang21@gmail.com>
2019-02-14 16:48:47 +0100

    Retoque de props
    Sprite de botón pulsado
    Sprite de muro bajado
    Sprite de hielo
    Prop de clonador

--- Fluff!/Assets/Scripts/Cinta.cs
+++ Fluff!/Assets/Scripts/Cinta.cs
@@ -7,16 +7,11 @@ public class Cinta : MonoBehaviour
     public float fuerza = 0f;
     public Vector2 direccion = Vector2.left;
 
-    void Start()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.layer == LayerMask.NameToLayer("Pelusas"))
         {
-            col.GetComponent<Rigidbody2D>().AddForce(direccion * fuerza);
+			col.GetComponent<Rigidbody2D>().velocity = (direccion * fuerza);
         }
     }
 }
