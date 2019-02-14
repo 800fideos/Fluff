3056cf4f4ea8efb4c9440d9a9926fc6615e72f44
Viejastirpe <danieljq1995@gmail.com>
2019-02-14 17:28:44 +0100

    Selección Mundo 3 Creado

---
+++ Fluff!/Assets/Scripts/CargaMundo3.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class CargaMundo3 : MonoBehaviour
+{
+	// Start is called before the first frame update
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+	public void CargaNivel13()
+	{
+		//IMPORTANTE PONER ARRIBA using UnityEngine.SceneManagement;//
+		SceneManager.LoadScene("Mudo3_Nivel1");
+	}
+
+	public void CargaNivel23()
+	{
+		//IMPORTANTE PONER ARRIBA using UnityEngine.SceneManagement;//
+		SceneManager.LoadScene("Mudo3_Nivel2");
+	}
+
+	public void CargaNivel33()
+	{
+		//IMPORTANTE PONER ARRIBA using UnityEngine.SceneManagement;//
+		SceneManager.LoadScene("Mudo3_Nivel3");
+	}
+
+
+	public void CargaNivel43()
+	{
+		//IMPORTANTE PONER ARRIBA using UnityEngine.SceneManagement;//
+		SceneManager.LoadScene("Mudo3_Nivel4");
+	}
+
+
+	public void CargaNivel53()
+	{
+		//IMPORTANTE PONER ARRIBA using UnityEngine.SceneManagement;//
+		SceneManager.LoadScene("Mudo3_Nivel5");
+	}
+}
