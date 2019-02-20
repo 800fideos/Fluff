9419aeae283fb04c76443da2579777a101481e22
Cooconuts <rafikyzhang21@gmail.com>
2019-02-20 17:46:36 +0100

    Retoque de props especiales
    Retoque animación del hielo
    Colliders del muelle

--- Fluff!/Assets/Scripts/Muelle.cs
+++ Fluff!/Assets/Scripts/Muelle.cs
@@ -5,26 +5,15 @@ using UnityEngine;
 public class Muelle : MonoBehaviour
 {
 	Animator animacion;
-	public GameObject desiredPosition;
-
-	public float fuerza = 0f;
-	public Vector2 direccion = Vector2.left;
-	public Vector3 distanciaParada;
 
     void Start()
     {
 		animacion = gameObject.GetComponent<Animator>();
-		//transform.position = new Vector3 (Mathf.Round (transform.position.x), Mathf.Round (transform.position.y), 0);
 
     }
 
-	private void OnCollisionEnter2D(Collision2D col)
+	public void ActivaAnimacion()
 	{
-		if (col.gameObject.layer == LayerMask.NameToLayer("Pelusas"))
-		{
-			col.gameObject.GetComponent<Rigidbody2D>().AddForce(direccion * fuerza);
-			col.gameObject.GetComponent<Movimiento> ().MoveryParar ((transform.position + distanciaParada));
-			animacion.SetTrigger ("rebote");
-		}
+		animacion.SetTrigger ("rebote");
 	}
 }
