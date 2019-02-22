e07a17abec01f0c98fc2946161b6a4bf3bd54011
Monchburg <37349527+Monchburg@users.noreply.github.com>
2019-02-22 19:09:31 +0100

    Raycast

--- Fluff!/Assets/Scripts/UnionPelusas.cs
+++ Fluff!/Assets/Scripts/UnionPelusas.cs
@@ -6,12 +6,16 @@ public class UnionPelusas : MonoBehaviour
 {
     Rigidbody2D rb;
 
+    
     bool golpeTop = false;
     bool golpeBot = false;
     bool golpeRight = false;
     bool golpeLeft = false;
     SpriteRenderer sr;
     public Sprite[] spritePelusa;
+
+    public bool pelusaUnida = false;
+    public GameObject padre;
 
     void Start()
     {
@@ -21,64 +25,136 @@ public class UnionPelusas : MonoBehaviour
 
     void Update()
     {
-        if (golpeTop)
+        if (pelusaUnida)
+        RevisarPelusasHijas();
+
+    }
+
+    private void CrearPadrePelusas()
+    {
+        padre = new GameObject();
+        padre.AddComponent<Movimiento>();
+        AniadirRigidBodyAPadre();
+    }
+
+    private void AniadirRigidBodyAPadre()
+    {
+        padre.AddComponent<Rigidbody2D>();
+        padre.GetComponent<Rigidbody2D>().gravityScale = 0;
+        padre.GetComponent<Rigidbody2D>().simulated = true;
+        padre.GetComponent<Rigidbody2D>().freezeRotation = true;
+        padre.GetComponent<Rigidbody2D>().mass = 0.1f;
+        padre.layer = LayerMask.NameToLayer("Pelusas");
+    }
+    
+    private void IniciarUniones()
+    {
+        pelusaUnida = true;
+        Destroy(rb);
+        CrearPadrePelusas();
+    }
+    
+
+    private void CambiarSprites(bool[] alrededorPelusas)
+    {
+
+    }
+
+    private bool LanzarRaycast(Vector2 direccion, Vector2 position)
+    {
+        Collider2D collider = Physics2D.Raycast(position, direccion, 0.1f).collider;
+
+
+        Debug.DrawRay(position, direccion, Color.red);
+
+        // DEBUG
+        if (collider != null)
         {
-            Debug.DrawRay(transform.position, Vector2.up, Color.red);
+            Debug.Log(gameObject.name + " etá detectando: " + collider.gameObject.name);
         }
-        else
+
+
+
+        return (collider != null && collider.gameObject.layer == LayerMask.NameToLayer("Pelusas"));
+    }
+
+    private bool LanzarRaycastArriba()
+    {
+        return LanzarRaycast(Vector2.up, new Vector2(transform.position.x, (transform.position.y + gameObject.GetComponent<Collider2D>().bounds.size.y / 2 + 0.1f)));
+    }
+
+    private bool LanzarRaycastDerecha()
+    {
+        return LanzarRaycast(Vector2.right, new Vector2(transform.position.x + gameObject.GetComponent<Collider2D>().bounds.size.x / 2 + 0.1f, transform.position.y));
+    }
+
+    private bool LanzarRaycastAbajo()
+    {
+        return LanzarRaycast(Vector2.down, new Vector2(transform.position.x, -(transform.position.y + gameObject.GetComponent<Collider2D>().bounds.size.y / 2 + 0.1f)));
+    }
+
+    private bool LanzarRaycastIzquierdo()
+    {
+        return LanzarRaycast(Vector2.left, new Vector2(-(transform.position.x + gameObject.GetComponent<Collider2D>().bounds.size.x / 2 + 0.1f), transform.position.y));
+    }
+
+    private void LanzarRaycastsAlrededor()
+    {
+        bool[] alrededorPelusas = new bool[4];
+        // 0 Arriba
+        // 1 derecha
+        // 2 Abajo
+        // 3 Izquierda
+
+        alrededorPelusas[0] = LanzarRaycastArriba();
+        alrededorPelusas[1] = LanzarRaycastDerecha();
+        alrededorPelusas[2] = LanzarRaycastAbajo();
+        alrededorPelusas[3] = LanzarRaycastIzquierdo();
+
+        Debug.Log("[" + gameObject.name + "]: " + "Colisión arriba --> " + alrededorPelusas[0]);
+        Debug.Log("[" + gameObject.name + "]: " + "Colisión derecha --> " + alrededorPelusas[1]);
+        Debug.Log("[" + gameObject.name + "]: " + "Colisión Abajo --> " + alrededorPelusas[2]);
+        Debug.Log("[" + gameObject.name + "]: " + "Colisión Izquierda --> " + alrededorPelusas[3]);
+
+        CambiarSprites(alrededorPelusas);
+    }
+
+    private void RevisarPelusasHijas()
+    {
+        for (int i = 0; i <  transform.parent.transform.childCount; i++)
         {
-            Debug.DrawRay(transform.position, Vector2.up, Color.yellow);
+            transform.parent.GetChild(i).GetComponent<UnionPelusas>().LanzarRaycastsAlrededor();
         }
-        
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-
-        if (col.gameObject.layer == LayerMask.NameToLayer("Pelusas") && col.transform.parent == null)
-        {
-            Destroy(rb);
-            transform.parent = col.transform;
-        }
-        else
-        {
-            rb.velocity = Vector2.zero;
-        }
-
-        RaycastHit2D hitTop = Physics2D.Raycast(transform.position, Vector2.up);
-
-      
-        Debug.Log("Did Hit");
-
-        if (hitTop.collider != null)
-        {
-            if (GetComponent<Collider>().gameObject.layer == LayerMask.NameToLayer ("Pelusas"))
+        if (col.gameObject.layer == LayerMask.NameToLayer("Pelusas")){
+            if (col.transform.parent == null &&
+                col.gameObject.GetComponent<UnionPelusas>() != null &&
+                !col.gameObject.GetComponent<UnionPelusas>().pelusaUnida)
             {
-                UnirPelusas();
+                if (rb != null) { // Primera vez que se une
+                    IniciarUniones();
+                }
+                UnirPelusa(col);
             }
+            else
+            {
+                transform.parent.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            }
+
+            RevisarPelusasHijas();
         }
     }
 
-    void UnirPelusas()
+    void UnirPelusa(Collision2D col)
     {
-        if (golpeTop)
-        {
-            sr.sprite = spritePelusa[0];
-        }
-        else if (golpeBot)
-            {
-                sr.sprite = spritePelusa[1];
-            }
-            else if (golpeLeft)
-                {
-                    sr.sprite = spritePelusa[2];
-                }
-                else if (golpeRight)
-                    {
-                        sr.sprite = spritePelusa[3];
-                    }
-                
-            
-        
+        Destroy(col.gameObject.GetComponent<Rigidbody2D>());
+        transform.parent = padre.transform;
+        col.transform.parent = padre.transform;
+        col.gameObject.GetComponent<UnionPelusas>().pelusaUnida = true;
+        col.gameObject.GetComponent<UnionPelusas>().padre = padre;
+        padre.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
 }
