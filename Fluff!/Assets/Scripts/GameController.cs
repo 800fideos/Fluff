0db94ba115d43921f6a1648a924fa83f60b80ba4
Viejastirpe <danieljq1995@gmail.com>
2019-02-26 16:32:35 +0100

    #114 Colocación de botones
    Crear los botones del menú completado y hacerlos funcionales

--- Fluff!/Assets/Scripts/GameController.cs
+++ Fluff!/Assets/Scripts/GameController.cs
@@ -11,7 +11,7 @@ public class GameController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+		Scene scene = SceneManager.GetActiveScene();
     }
 
     // Update is called once per frame
