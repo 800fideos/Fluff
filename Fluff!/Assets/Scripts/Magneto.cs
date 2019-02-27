d1ac1cfeeee0dae59d2d97ca9fadbb003bde6cbe
Monchburg <37349527+Monchburg@users.noreply.github.com>
2019-02-27 18:42:47 +0100

    #65 Script Personaje Electrico
    Por acabar

--- Fluff!/Assets/Scripts/Magneto.cs
+++ Fluff!/Assets/Scripts/Magneto.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class Magneto : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Rigidbody2D rb;
+
+    public float fuerzaAtraccion = 5f;
+    bool atraeDerecha = false;
+    bool atraeIzquierda = false;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        LanzarRaycast();
+    }
+
+    private void AtraerPelusa()
+    {
+        if (gameObject.layer == LayerMask.NameToLayer("Pelusas"))
+        {
+            if (atraeDerecha)
+            {
+                rb.velocity = (Vector2.left * fuerzaAtraccion);
+            }
+            if (atraeIzquierda)
+            {
+                rb.velocity = (Vector2.right * fuerzaAtraccion);
+            }  
+        }
+    }
+
+    private void LanzarRaycast()
+    {
+        RaycastHit2D hitDerecha = Physics2D.Raycast(new Vector2 (transform.position.x + gameObject.GetComponent<Collider2D>().bounds.size.x / 2 + 0.1f, transform.position.y), Vector2.right);
+        RaycastHit2D hitIzquierda = Physics2D.Raycast(new Vector2 (-(transform.position.x + gameObject.GetComponent<Collider2D>().bounds.size.x / 2 + 0.1f), transform.position.y), Vector2.left);
+
+        //Debug.DrawRay(position, direction, Color.blue);
         
+        if (hitDerecha.collider != null)
+        {
+            atraeDerecha = true;
+            AtraerPelusa();
+        }
+        if (hitIzquierda.collider != null)
+        {
+            atraeIzquierda = true;
+            AtraerPelusa();
+        }
     }
 }
+
--- Fluff!/Assets/Scripts/UnionPelusas.cs
+++ Fluff!/Assets/Scripts/UnionPelusas.cs
@@ -25,8 +25,6 @@ public class UnionPelusas : MonoBehaviour
 
     void Update()
     {
-        //if (pelusaUnida)
-            
 
     }
 
