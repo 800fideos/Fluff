8f9ea7191a8e213df3f04cb2fa252138b98d5679
Monchburg <37349527+Monchburg@users.noreply.github.com>
2019-03-07 18:31:56 +0100

    Union Padres (No hecha)

---
+++ Fluff!/Assets/Scripts/UnionPadres.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnionPadres : MonoBehaviour
+{
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void UnirPadres(GameObject OtroPadre)
+    {
+        for (int i = 0; i < OtroPadre.transform.childCount; i++)
+        {
+            OtroPadre.transform.GetChild(0).transform.parent = transform.parent;
+        }
+        Destroy(OtroPadre);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.parent == null &&
+                col.transform.childCount > 0 &&
+                !GetComponent<Movimiento>().enMovimiento)
+        {
+            UnirPadres(col.gameObject);
+        }
+    }
+}
