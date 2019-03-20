b9022dcbb84c8460e4d0943007425ec68b20851b
Monchburg <37349527+Monchburg@users.noreply.github.com>
2019-03-20 02:45:48 +0100

    UnionPadres

--- Fluff!/Assets/Scripts/UnionPadres.cs
+++ Fluff!/Assets/Scripts/UnionPadres.cs
@@ -5,26 +5,55 @@ using UnityEngine;
 public class UnionPadres : MonoBehaviour
 {
     Rigidbody2D rb;
+    GameObject padreDePadres;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void CreaPadreDePadres()
+    {
+        padreDePadres = new GameObject();
+        padreDePadres.AddComponent<Movimiento>();
+        padreDePadres.AddComponent<UnionPadres>();
+        padreDePadres.gameObject.tag = "Unido";
+        AniadirRigidBodyAPadreDePadres();
+        AniadirAudioSourceAPadreDePadres();
+
+        /**/padreDePadres.AddComponent<Collider2D>();
+    }
+    private void AniadirRigidBodyAPadreDePadres()
+    {
+        padreDePadres.AddComponent<Rigidbody2D>();
+        padreDePadres.GetComponent<Rigidbody2D>().gravityScale = 0;
+        padreDePadres.GetComponent<Rigidbody2D>().simulated = true;
+        padreDePadres.GetComponent<Rigidbody2D>().freezeRotation = true;
+        padreDePadres.GetComponent<Rigidbody2D>().mass = 0.1f;
+        padreDePadres.layer = LayerMask.NameToLayer("Pelusas");
+    }
+
+    private void AniadirAudioSourceAPadreDePadres()
+    {
+        padreDePadres.AddComponent<AudioSource>();
+        padreDePadres.GetComponent<Movimiento>().sonidoChoque = GetComponent<Movimiento>().sonidoChoque;
+    }
+
     private void UnirPadres(GameObject OtroPadre)
     {
-        for (int i = 0; i < OtroPadre.transform.childCount; i++)
-        {
-            OtroPadre.transform.GetChild(0).transform.parent = transform.parent;
-        }
-        Destroy(OtroPadre);
+        Destroy(OtroPadre.transform.GetComponent<Rigidbody2D>());
+        CreaPadreDePadres();
+        transform.parent = padreDePadres.transform;
+        OtroPadre.transform.parent = padreDePadres.transform;
+        OtroPadre.gameObject.GetComponent<UnionPelusas>().pelusaUnida = true;
+        OtroPadre.gameObject.GetComponent<UnionPelusas>().padre = padreDePadres;
+        OtroPadre.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        OtroPadre.transform.gameObject.tag = "Unido";
     }
 
     private void OnCollisionEnter2D(Collision2D col)
     {
-        if (col.transform.parent == null &&
-                col.transform.childCount > 0 &&
-                !GetComponent<Movimiento>().enMovimiento)
+        if (col.transform.parent != transform && rb != null)
         {
             UnirPadres(col.gameObject);
         }
