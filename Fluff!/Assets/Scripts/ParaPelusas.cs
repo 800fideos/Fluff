30aeb3da3f2dddba65a6f80eebc892afafa146df
Monchburg <37349527+Monchburg@users.noreply.github.com>
2019-03-20 14:04:55 +0100

    Comentarios

--- Fluff!/Assets/Scripts/ParaPelusas.cs
+++ Fluff!/Assets/Scripts/ParaPelusas.cs
@@ -1,4 +1,13 @@
-﻿using System.Collections;
+﻿/* ParaPelusas.cs
+ * 19/03/2019
+ * Versión: 0.3
+ * Realizado por @monchburg (Ramón González)
+ * Comentado por @monchburg (Ramón González)
+ * Script que para las pelusas en los charcos.
+ * 
+ * */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,14 +25,15 @@ public class ParaPelusas : MonoBehaviour
         
     }
 
+    //Función que controla las colisiones con el charco.
     private void OnTriggerEnter2D(Collider2D col)
     {
-
+        // Si el objeto que colisiona es una pelusa, no tiene padre y no está unida con nada.
         if (col.gameObject.layer == LayerMask.NameToLayer("Pelusas") && col.gameObject != miPadre && !col.gameObject.GetComponent<UnionPelusas>().pelusaUnida)
         {
-            col.transform.position = transform.position;
-            col.GetComponent<Rigidbody2D>().velocity = (col.transform.position * 0);
-            col.GetComponent<Movimiento>().enMovimiento = false;
+            col.transform.position = transform.position; // Iguala la posición de la pelusa a la del charco. 
+            col.GetComponent<Rigidbody2D>().velocity = (col.transform.position * 0); // Frena a la pelusa que ha entrado en el charco.
+            col.GetComponent<Movimiento>().enMovimiento = false; // Desactiva el booleano para que la pelusa pueda moverse de nuevo. 
         }
     }
     public void SetPadre(GameObject padre)
